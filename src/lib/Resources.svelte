cd14538cc0ceeb4f92dfede0e5f09666373adabb
iamdowantcoffee <94763189+iamdowantcoffee@users.noreply.github.com>
2022-09-24 17:04:34 -0500

    Create Resources.svelte

---
+++ src/lib/Resources.svelte
@@ -0,0 +1,108 @@
+<script>
+    import List, { Item, Graphic, Separator, Text } from '@smui/list';
+import { dynasty } from './utils/helper';
+
+    const today = new Date();
+    const resources = [
+        {
+            name: "The Commissioner's Report",
+            url: "https://open.spotify.com/show/0D4VQy1dmncxfVmwyVVfLj?si=8feb6c82697a479f",
+            icon: "podcasts",
+            premium: false,
+            separator: true,
+        },
+        {
+            name: "The Fantasy Footballers",
+            url: "https://open.spotify.com/show/5RaNsb5sKEBleahQa4MVC5?si=63eb9d2e317c421d",
+            icon: "podcasts",
+            premium: false,
+            separator: true,
+        },
+    ];
+</script>
+
+<style>
+    .pageBody {
+        position: relative;
+        z-index: 1;
+    }
+
+    :global(.list) {
+        width: 90%;
+        max-width: 800px;
+        border: 1px solid
+        var(--mdc-theme-text-hint-on-background, var(--d7d7d7));
+        margin: 15px auto;
+        padding: 0 !important;
+        box-shadow: 0px 3px 3px -2px var(--boxShadowOne), 0px 3px 4px 0px var(--boxShadowTwo), 0px 1px 8px 0px var(--boxShadowThree);
+    }
+
+
+    :global(.linkText) {
+        font-size: 1.1em;
+    }
+
+    a {
+        color: var(--g555);
+        font-size: 1.1em;
+        text-decoration: none;
+    }
+
+    h4 {
+        text-align: center;
+    }
+
+    .disclaimer {
+        color: #888;
+        text-align: center;
+    }
+</style>
+
+<div class="pageBody">
+    <div class="banner">
+        <h4>Helpful Resources</h4>
+    </div>
+
+    <List class="list" dense>
+        {#each resources as resource}
+            {#if resource.dynastyOnly && dynasty}
+                <a target="_blank" href="{resource.url}">
+                    <Item>
+                        {#if resource.icon}
+                            <Graphic class="material-icons">{resource.icon}</Graphic>
+                        {/if}
+                        <Text class="linkText">{resource.name}{resource.premium ? "*" : ""}</Text>
+                    </Item>
+                    {#if resource.separator}
+                        <Separator />
+                    {/if}
+                </a>
+            {:else if resource.redraftOnly && !dynasty}
+                <a target="_blank" href="{resource.url}">
+                    <Item>
+                        {#if resource.icon}
+                            <Graphic class="material-icons">{resource.icon}</Graphic>
+                        {/if}
+                        <Text class="linkText">{resource.name}{resource.premium ? "*" : ""}</Text>
+                    </Item>
+                    {#if resource.separator}
+                        <Separator />
+                    {/if}
+                </a>
+            {:else if !resource.redraftOnly && !resource.dynastyOnly}
+                <a target="_blank" href="{resource.url}">
+                    <Item>
+                        {#if resource.icon}
+                            <Graphic class="material-icons">{resource.icon}</Graphic>
+                        {/if}
+                        <Text class="linkText">{resource.name}{resource.premium ? "*" : ""}</Text>
+                    </Item>
+                    {#if resource.separator}
+                        <Separator />
+                    {/if}
+                </a>
+            {/if}
+        {/each}
+    </List>
+    <p class="disclaimer">Enjoy.</p>
+</div>
